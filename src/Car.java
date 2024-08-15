0f18e595cc17274098d6d7fffa984a9666970df0
citrinitas <daniargizatullin8@gmail.com>
2024-08-15 14:01:19 +0500

    g

---
+++ src/Car.java
@@ -0,0 +1,29 @@
+public class Car implements Vehicle {
+
+    private String modelName;
+    private int wheelsCount;
+
+    public Car(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    @Override
+    public String getModelName() {
+        return modelName;
+    }
+
+    @Override
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
